79099eef969a9aff3c0836a96d9e301b722c9052
Jonathan Pastor <jonathancmoa@gmail.com>
2018-08-27 01:04:59 +0200

    add a setup.py file to enable the use of libopenttd as a library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='libopenttd',
+      version='0.1.0',
+      description='Python implementation of OpenTTD\'s network protocols',
+      license='Creative Commons',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      data_files=[],
+      zip_safe=False)
